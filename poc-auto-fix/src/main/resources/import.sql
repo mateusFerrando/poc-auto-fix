64c9136401d7439364bad4a9a61410081db8dc35
mateusFerrando <34379304+mateusFerrando@users.noreply.github.com>
2026-02-03 16:57:21 -0300

    Delete poc-auto-fix directory

--- poc-auto-fix/src/main/resources/import.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL
-);
-
-INSERT INTO users (name, email) VALUES ('admin', 'admin@example.com');
-INSERT INTO users (name, email) VALUES ('user', 'user@example.com');
